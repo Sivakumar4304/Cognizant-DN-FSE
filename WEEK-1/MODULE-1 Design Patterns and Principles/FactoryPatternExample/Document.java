985352310edbff5c4257351500d1aea9af7fa692
Sivakumar4304 <sivakumarboda0@gmail.com>
2026-06-26 11:39:29 +0530

    patterns and principles

--- WEEK-1/MODULE-1 Design Patterns and Principles/FactoryPatternExample/Document.java
+++
@@ -1,8 +0,0 @@
-/**
- * Product Interface
- * Represents a generic document.
- */
-public interface Document {
-
-    void open();
-}
--- WEEK-1/MODULE-1 Design Patterns and Principles/FactoryPatternExample/DocumentFactory.java
+++
@@ -1,8 +0,0 @@
-/**
- * Creator Abstract Class
- * Declares the factory method.
- */
-public abstract class DocumentFactory {
-
-    public abstract Document createDocument();
-}
