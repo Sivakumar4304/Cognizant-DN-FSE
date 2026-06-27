7c44ff19601e19626a3f95a74568b179a212cf82
Sivakumar4304 <sivakumarboda0@gmail.com>
2026-06-27 22:25:27 +0530

    DESIGN PATTERNS AND SOLID PRINCIPLES

---
+++ week 1/DESIGN patterns/StrategyPatternExample.java
@@ -0,0 +1,38 @@
+interface PaymentStrategy {
+    void pay();
+}
+
+class CreditCardPayment implements PaymentStrategy {
+    public void pay() {
+        System.out.println("Paid using credit card");
+    }
+}
+
+class PayPalPayment implements PaymentStrategy {
+    public void pay() {
+        System.out.println("Paid using PayPal");
+    }
+}
+
+class PaymentContext {
+    PaymentStrategy ps;
+
+    PaymentContext(PaymentStrategy ps) {
+        this.ps = ps;
+    }
+
+    void show() {
+        ps.pay();
+    }
+}
+
+public class StrategyPatternExample {
+    public static void main(String[] args) {
+
+        PaymentContext pc1 = new PaymentContext(new CreditCardPayment());
+        pc1.show();
+
+        PaymentContext pc2 = new PaymentContext(new PayPalPayment());
+        pc2.show();
+    }
+}
